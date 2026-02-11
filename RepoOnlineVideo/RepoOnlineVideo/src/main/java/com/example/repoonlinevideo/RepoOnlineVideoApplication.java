7f92a6d6d6ad4392bd9597a07aafa1f623fe3079
Kanghee <kh63167574@gmail.com>
2026-02-11 22:37:40 +0900

    feat: 기본세팅

---
+++ RepoOnlineVideo/RepoOnlineVideo/src/main/java/com/example/repoonlinevideo/RepoOnlineVideoApplication.java
@@ -0,0 +1,13 @@
+package com.example.repoonlinevideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoOnlineVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoOnlineVideoApplication.class, args);
+    }
+
+}
---
+++ RepoOnlineVideo/RepoOnlineVideo/src/test/java/com/example/repoonlinevideo/RepoOnlineVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repoonlinevideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoOnlineVideoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
